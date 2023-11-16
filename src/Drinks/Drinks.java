034106881038e4e16ad1c08981d4fa684261e79d
PinkyBabe <piodos.jam22@gmail.com>
2023-11-16 16:23:31 +0800

    Drinks Package Added

---
+++ src/Drinks/Drinks.java
@@ -0,0 +1,13 @@
+
+package Drinks;
+
+public class Drinks {
+
+   public static void main(String[] args) {
+       
+       DrinksSelected menu = new DrinksSelected("Coke",25);
+   
+       menu.a1();
+       menu.a2();
+   }
+}
---
+++ src/Drinks/DrinksSelected.java
@@ -0,0 +1,20 @@
+
+package Drinks;
+
+class DrinksSelected {
+    String a1;
+    double a2;
+ 
+    DrinksSelected(String a1, double a2) {
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+    
+    void a1(){
+        System.out.println(a1);
+    }
+    void a2(){
+        System.out.println(a2);
+    }
+    
+}
