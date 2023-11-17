5cdc52c0b41701480a04c004739b119c8c9a462f
PinkyBabe <piodos.jam22@gmail.com>
2023-11-17 13:52:46 +0800

    StudentProfile Package Added

---
+++ src/StudentProfile/Main.java
@@ -0,0 +1,16 @@
+package StudentProfile;
+
+class Main {
+    
+    
+    public static void main(String[] args) {
+       Information i = new Information();
+       
+       i.setName();
+       i.setSection();
+       i.setMotto();
+       i.Info();
+        
+    }
+    
+}
