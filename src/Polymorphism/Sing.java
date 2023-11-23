6588cc93ae4aa1e5a8b94174e6ea58cecba6bb12
PinkyBabe <piodos.jam22@gmail.com>
2023-11-23 16:15:38 +0800

    Sing.java added

---
+++ src/Polymorphism/Sing.java
@@ -0,0 +1,13 @@
+public class Sing extends Rey{
+    Sing(String name){
+        super(name);
+    }
+
+    void Song1(){
+        System.out.println(name+" sings Paubaya");
+    }
+
+    void Song2(){
+        System.out.println(name+" is Tired and starts moaning");
+    }
+}
