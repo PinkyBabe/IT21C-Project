b34dbdd18e0f1529578a4645095e5fda1587708e
PinkyBabe <piodos.jam22@gmail.com>
2023-11-23 16:14:07 +0800

    Rey.java Added

---
+++ src/Polymorphism/Polymorphism.java
@@ -0,0 +1,7 @@
+class Polymorphism{
+
+    public static void main(String[] args){
+        Rey rey = new Rey();
+        
+    }
+}
---
+++ src/Polymorphism/Rey.java
@@ -0,0 +1,7 @@
+public class Rey {
+    String name;
+
+    Rey(String name){
+        this.name = name;
+    }
+}
