90a4871e24d015d26c54393f4200a49cf3579a55
PinkyBabe <piodos.jam22@gmail.com>
2023-11-23 16:15:08 +0800

    Dance.java added

---
+++ src/Polymorphism/Dance.java
@@ -0,0 +1,15 @@
+class Dance extends Rey {
+    
+    Dance(String name){
+        super(name);
+    }
+
+    void Step1(){
+        System.out.println(name+" did a back flip");
+    }
+
+    void Step2(){
+        System.out.println(name+" did a Tiktok");
+    }
+
+}
