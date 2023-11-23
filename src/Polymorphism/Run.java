95c52f2430a4859ea1477f81143766145a9c936d
PinkyBabe <piodos.jam22@gmail.com>
2023-11-23 16:18:08 +0800

    Run.java added

---
+++ src/Polymorphism/Run.java
@@ -0,0 +1,14 @@
+public class Run extends Rey{
+    
+    Run(String name){
+        super(name);
+    }
+
+    void run1(){
+        System.out.println(name+ "is running");
+    }
+
+    void run2(){
+        System.out.println(name+ "is running backwards");
+    }
+}
