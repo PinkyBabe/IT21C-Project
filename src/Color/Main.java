c1f03f3297a0c5272016bc8880d47cfde1b29896
PinkyBabe <piodos.jam22@gmail.com>
2023-11-24 13:25:17 +0800

    Color Package got Added

---
+++ src/Color/Main.java
@@ -0,0 +1,15 @@
+package Color;
+
+public class Main {
+   
+    public static void main(String[] args) {
+
+        Red bag = new Red("Levis Bag");
+        Blue shoes = new Blue("Nike Shoes");
+        Yellow shirt = new Yellow("Oxygen Shirt");
+        
+        bag.Color();
+        shoes.Color();
+        shirt.Color();
+    }
+}
